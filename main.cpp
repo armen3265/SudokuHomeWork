49fc325a8c00e780c3210400b30f26b613290c73
armen3265 <74138554+armen3265@users.noreply.github.com>
2021-01-03 18:36:25 +0400

    Update main.cpp
    Add exception.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
+#include <string>
 #include "sudoku.h"
+
+using namespace std;
 
 int main()
 {
-	Sudoku sudoku;
+	try
+	{
+		Sudoku sudoku;
+		std::string board[sudoku.size];
 
-	sudoku.enterDigits();
-	sudoku.solve();
-	sudoku.print();
+		std::cout << "Please add numbers from 1 to 9." << "\n\n";
+		for (int i = 0; i < sudoku.size; i++)
+		{
+			std::getline(std::cin, board[i]);
+		}
+		std::cout << "Thanks!All numbers have been added successfully." << "\n\n";
+
+		sudoku.enterDigits(board);
+		sudoku.solve();
+		std::cout<<sudoku.answer()<<endl;
+	}
+	catch (const std::exception& ex)
+	{
+		std::cout << ex.what() << "\n";
+	}
 }
